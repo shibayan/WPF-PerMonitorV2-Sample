e92592fce250d1080ae6380e9c53b2c3d072df66
shibayan <me@shibayan.jp>
2019-06-11 20:26:00 +0900

    Adding project

---
+++ WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //protected override void OnSourceInitialized(EventArgs e)
+        //{
+        //    base.OnSourceInitialized(e);
+
+        //    var hMonitor = NativeMethods.MonitorFromWindow(NativeMethods.GetForegroundWindow(), Consts.MONITOR_DEFAULTTOPRIMARY);
+
+        //    NativeMethods.GetDpiForMonitor(hMonitor, Consts.MDT_EFFECTIVE_DPI, out var dpiX, out var dpiY);
+
+        //    var dpiFactorX = dpiX / 96.0;
+        //    var dpiFactorY = dpiY / 96.0;
+
+        //    var physicalWidth = (int)Math.Round(Width * dpiFactorX);
+        //    var physicalHeight = (int)Math.Round(Height * dpiFactorY);
+
+        //    var monitorInfo = new MONITORINFO
+        //    {
+        //        cbSize = Marshal.SizeOf<MONITORINFO>()
+        //    };
+
+        //    NativeMethods.GetMonitorInfo(hMonitor, ref monitorInfo);
+
+        //    var x = monitorInfo.rcWork.x + (monitorInfo.rcWork.cx - monitorInfo.rcWork.x - physicalWidth) / 2;
+        //    var y = monitorInfo.rcWork.y + (monitorInfo.rcWork.cy - monitorInfo.rcWork.y - physicalHeight) / 2;
+
+        //    var hwnd = new WindowInteropHelper(this).Handle;
+
+        //    NativeMethods.SetWindowPos(hwnd, IntPtr.Zero, x, y, 0, 0, Consts.SWP_NOSIZE);
+        //}
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            var hwnd = new WindowInteropHelper(this).Handle;
+
+            var hMonitor = NativeMethods.MonitorFromWindow(hwnd, Consts.MONITOR_DEFAULTTOPRIMARY);
+
+            var monitorInfo = new MONITORINFO
+            {
+                cbSize = Marshal.SizeOf<MONITORINFO>()
+            };
+
+            NativeMethods.GetMonitorInfo(hMonitor, ref monitorInfo);
+
+            Left = monitorInfo.rcWork.x + (monitorInfo.rcWork.cx - monitorInfo.rcWork.x - Width) / 2;
+            Top = monitorInfo.rcWork.y + (monitorInfo.rcWork.cy - monitorInfo.rcWork.y - Height) / 2;
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            var hwnd = new WindowInteropHelper(this).Handle;
+
+            var hMonitor = NativeMethods.MonitorFromWindow(hwnd, Consts.MONITOR_DEFAULTTOPRIMARY);
+
+            var monitorInfo = new MONITORINFO
+            {
+                cbSize = Marshal.SizeOf<MONITORINFO>()
+            };
+
+            NativeMethods.GetMonitorInfo(hMonitor, ref monitorInfo);
+
+            var x = monitorInfo.rcWork.x + (monitorInfo.rcWork.cx - monitorInfo.rcWork.x - Width) / 2;
+            var y = monitorInfo.rcWork.y + (monitorInfo.rcWork.cy - monitorInfo.rcWork.y - Height) / 2;
+
+            NativeMethods.SetWindowPos(hwnd, IntPtr.Zero, (int)x, (int)y, 0, 0, Consts.SWP_NOSIZE);
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            var hwnd = new WindowInteropHelper(this).Handle;
+
+            var hMonitor = NativeMethods.MonitorFromWindow(hwnd, Consts.MONITOR_DEFAULTTOPRIMARY);
+
+            NativeMethods.GetDpiForMonitor(hMonitor, Consts.MDT_EFFECTIVE_DPI, out var dpiX, out var dpiY);
+
+            var dpiFactorX = dpiX / 96.0;
+            var dpiFactorY = dpiY / 96.0;
+
+            var physicalWidth = (int)Math.Round(Width * dpiFactorX);
+            var physicalHeight = (int)Math.Round(Height * dpiFactorY);
+
+            var monitorInfo = new MONITORINFO
+            {
+                cbSize = Marshal.SizeOf<MONITORINFO>()
+            };
+
+            NativeMethods.GetMonitorInfo(hMonitor, ref monitorInfo);
+
+            Left = monitorInfo.rcWork.x + (monitorInfo.rcWork.cx - monitorInfo.rcWork.x - physicalWidth) / 2;
+            Top = monitorInfo.rcWork.y + (monitorInfo.rcWork.cy - monitorInfo.rcWork.y - physicalHeight) / 2;
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            var hwnd = new WindowInteropHelper(this).Handle;
+
+            var hMonitor = NativeMethods.MonitorFromWindow(hwnd, Consts.MONITOR_DEFAULTTOPRIMARY);
+
+            NativeMethods.GetDpiForMonitor(hMonitor, Consts.MDT_EFFECTIVE_DPI, out var dpiX, out var dpiY);
+
+            var dpiFactorX = dpiX / 96.0;
+            var dpiFactorY = dpiY / 96.0;
+
+            var physicalWidth = (int)Math.Round(Width * dpiFactorX);
+            var physicalHeight = (int)Math.Round(Height * dpiFactorY);
+
+            var monitorInfo = new MONITORINFO
+            {
+                cbSize = Marshal.SizeOf<MONITORINFO>()
+            };
+
+            NativeMethods.GetMonitorInfo(hMonitor, ref monitorInfo);
+
+            var x = monitorInfo.rcWork.x + (monitorInfo.rcWork.cx - monitorInfo.rcWork.x - physicalWidth) / 2;
+            var y = monitorInfo.rcWork.y + (monitorInfo.rcWork.cy - monitorInfo.rcWork.y - physicalHeight) / 2;
+
+            NativeMethods.SetWindowPos(hwnd, IntPtr.Zero, x, y, 0, 0, Consts.SWP_NOSIZE);
+        }
+    }
+}
---
+++ WpfApp4/NativeMethods.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace WpfApp4
+{
+    internal static class NativeMethods
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr MonitorFromWindow(IntPtr hwnd, int dwFlags);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetMonitorInfo(IntPtr hMonitor, ref MONITORINFO lpmi);
+
+        [DllImport("shcore.dll")]
+        public static extern int GetDpiForMonitor(IntPtr hMonitor, int dpiType, out uint dpiX, out uint dpiY);
+
+        [DllImport("user32.dll")]
+        public static extern int SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int cx, int cy, uint uFlags);
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct MONITORINFO
+    {
+        public int cbSize;
+        public RECT rcMonitor;
+        public RECT rcWork;
+        public int dwFlags;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct RECT
+    {
+        public int x;
+        public int y;
+        public int cx;
+        public int cy;
+
+        public RECT(int x, int y, int cx, int cy)
+        {
+            this.x = x;
+            this.y = y;
+            this.cx = cx;
+            this.cy = cy;
+        }
+    }
+
+    internal static class Consts
+    {
+        public const int MONITOR_DEFAULTTOPRIMARY = 1;
+        public const int MONITOR_DEFAULTTONEAREST = 2;
+
+        public const int DPI_AWARENESS_CONTEXT_UNAWARE = 16;
+        public const int DPI_AWARENESS_CONTEXT_SYSTEM_AWARE = 17;
+        public const int DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE = 18;
+        public const int DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2 = 34;
+
+        public const int MDT_EFFECTIVE_DPI = 0;
+        public const int MDT_ANGULAR_DPI = 1;
+        public const int MDT_RAW_API = 3;
+
+        public const int SWP_NOACTIVATE = 0x0010;
+        public const int SWP_NOSIZE = 0x0001;
+        public const int SWP_NOZORDER = 0x0004;
+    }
+}
